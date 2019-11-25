bcd816635e954db35259d38a4c674b917c2458d5
Tom Cybart <tom.cybart@hochschule-stralsund.de>
2019-11-25 17:37:20 +0100

    Klasse Item eingefügt, die dazugehörigen Setter und Getter und die Override Methode

---
+++ D3/src/Item.java
@@ -0,0 +1,64 @@
+public class Item {
+
+    private String title;
+    private String link;
+    private String pubDate;
+    private String content;
+    private String description;
+    private String guid;
+
+    public Item() {}
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getPubDate() {
+        return pubDate;
+    }
+
+    public void setPubDate(String pubDate) {
+        this.pubDate = pubDate;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getGuid() {
+        return guid;
+    }
+
+    public void setGuid(String guid) {
+        this.guid = guid;
+    }
+
+    @Override
+    public String toString() {
+        return "[[" + this.title + " ] [ " + this.link + " ] [ " + this.pubDate + " ] [ " + this.content + " ] [ " + description + " ] [ " + guid + " ]]";
+    }
+}
--- D3/src/Main.java
+++ D3/src/Main.java
@@ -1,3 +1,8 @@
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+import org.xml.sax.helpers.XMLReaderFactory;
+
 import java.net.*;
 import java.io.*;
 import java.util.Date;
@@ -15,6 +20,30 @@ public class Main {
             while ((line = input.readLine()) != null) System.out.println(line);
         } catch (Exception e) {
             System.out.println(e.toString());
+        }
+
+        try {
+            // XMLReader erzeugen
+            XMLReader xmlReader = XMLReaderFactory.createXMLReader();
+
+            // Pfad zur XML Datei
+            FileReader reader = new FileReader("X:\\personen.xml");
+            InputSource inputSource = new InputSource(reader);
+
+            // DTD kann optional übergeben werden
+            // inputSource.setSystemId("X:\\personen.dtd");
+
+            // PersonenContentHandler wird übergeben
+            xmlReader.setContentHandler(new PersonenContentHandler());
+
+            // Parsen wird gestartet
+            xmlReader.parse(inputSource);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
         }
     }
 }
